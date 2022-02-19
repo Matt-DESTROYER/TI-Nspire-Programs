d54c1e78b16a64589ceb3e00f103b38936d01c11
Matthew James <69026158+Matt-DESTROYER@users.noreply.github.com>
2022-02-20 10:14:45 +1100

    Create scripts.mjs

---
+++ Maze-Escape-Level-Editor/Published-Levels/scripts.mjs
@@ -0,0 +1,23 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
+import { getFirestore, collection, getDocs } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
+
+const firebaseConfig = {
+	apiKey: "AIzaSyAYc2AtdxlSEkD_VrGaIiKjOv0B3xD7uSs",
+	authDomain: "ti-nspire-programs-database.firebaseapp.com",
+	projectId: "ti-nspire-programs-database",
+	storageBucket: "ti-nspire-programs-database.appspot.com",
+	messagingSenderId: "1026879731613",
+	appId: "1:1026879731613:web:45cdecd4e9ed5dc57ba174"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore();
+
+async function GetCollection(collectionName) {
+	try {
+		return await getDocs(collection(db, collectionName));
+	} catch (err) {
+		return err;
+	}
+}
