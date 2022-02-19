466a4668ffe3bad9a870423c4bc8e8721f3827cf
Matthew James <69026158+Matt-DESTROYER@users.noreply.github.com>
2022-02-20 10:27:16 +1100

    Update scripts.mjs

--- Maze-Escape-Level-Editor/Published-Levels/scripts.mjs
+++ Maze-Escape-Level-Editor/Published-Levels/scripts.mjs
@@ -22,7 +22,48 @@ async function GetCollection(collectionName) {
 	}
 }
 
+const body = document.body || document.getElementsByTagName("body")[0];
 (await GetCollection("Levels")).forEach(async (level) => {
 	const data = level.data();
-	console.log(data);
+	body.appendChild(document.createElement("br"));
+	body.appendChild(document.createElement("br"));
+	const level = document.createElement("div");
+	level.classList.add("program-container");
+	const heading = document.createElement("h2");
+	heading.textContent = data.levelName;
+	level.appendChild(heading);
+	const author = document.createElement("p");
+	author.textContent = data.author;
+	level.appendChild(author);
+	const levelDisplay = document.createElement("canvas");
+	levelDisplay.width = 320;
+	levelDisplay.height = 240;
+	const ctx = levelDisplay.getContext("2d");
+	ctx.fillStyle = "#ffffff";
+	ctx.fillRect(0, 0, levelDisplay.width, levelDisplay.height);
+	ctx.beginPath();
+	for (let y = 0; y <= 24; y++) {
+		ctx.moveTo(0, y * 10);
+		ctx.lineTo(levelDisplay.width, y * 10);
+	}
+	for (let x = 0; x <= 32; x++) {
+		ctx.moveTo(x * 10, 0);
+		ctx.lineTo(x * 10, levelDisplay.height);
+	}
+	ctx.stroke();
+	ctx.closePath();
+	for (let y = 0; y < 24; y++) {
+		for (let x = 0; x < 32; x++) {
+			if (data.levelData[y][x] === "#") {
+				ctx.fillStyle = "#000000";
+				ctx.fillRect(x * 10, y * 10, 10, 10);
+			} else if (data.levelData[y][x] === "@") {
+				ctx.fillStyle = "#0000ff";
+				ctx.fillRect(x * 10, y * 10, 10, 10);
+			} else if (data.levelData[y][x] === "*") {
+				ctx.fillStyle = "#008000";
+				ctx.fillRect(x * 10, y * 10, 10, 10);
+			}
+		}
+	}
 });
