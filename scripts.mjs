4182285d23d5155975319b6b3f6710bbd8713533
Matthew James <69026158+Matt-DESTROYER@users.noreply.github.com>
2022-02-20 11:56:41 +1100

    Create scripts.mjs

---
+++ scripts.mjs
@@ -0,0 +1,13 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.7/firebase-app.js";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAYc2AtdxlSEkD_VrGaIiKjOv0B3xD7uSs",
+  authDomain: "ti-nspire-programs-database.firebaseapp.com",
+  projectId: "ti-nspire-programs-database",
+  storageBucket: "ti-nspire-programs-database.appspot.com",
+  messagingSenderId: "1026879731613",
+  appId: "1:1026879731613:web:03c983f8072ebae67ba174"
+};
+
+const app = initializeApp(firebaseConfig);
