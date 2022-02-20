872b51f0fe1778c3f7d7689a6efbc3f7b4136b3c
Matthew James <69026158+Matt-DESTROYER@users.noreply.github.com>
2022-02-20 17:26:24 +1100

    Update scripts.mjs

--- scripts.mjs
+++ scripts.mjs
@@ -2,12 +2,12 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.7/firebase
 import { getFirestore, collection, doc, getDoc } from "https://www.gstatic.com/firebasejs/9.6.7/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAYc2AtdxlSEkD_VrGaIiKjOv0B3xD7uSs",
-  authDomain: "ti-nspire-programs-database.firebaseapp.com",
-  projectId: "ti-nspire-programs-database",
-  storageBucket: "ti-nspire-programs-database.appspot.com",
-  messagingSenderId: "1026879731613",
-  appId: "1:1026879731613:web:03c983f8072ebae67ba174"
+	apiKey: "AIzaSyAYc2AtdxlSEkD_VrGaIiKjOv0B3xD7uSs",
+	authDomain: "ti-nspire-programs-database.firebaseapp.com",
+	projectId: "ti-nspire-programs-database",
+	storageBucket: "ti-nspire-programs-database.appspot.com",
+	messagingSenderId: "1026879731613",
+	appId: "1:1026879731613:web:03c983f8072ebae67ba174"
 };
 
 const app = initializeApp(firebaseConfig);
@@ -31,6 +31,19 @@ programs.forEach(async (program) => {
 	programsContainer.appendChild(document.createElement("br"));
 	programsContainer.appendChild(document.createElement("br"));
 	const div = document.createElement("div");
+	programsContainer.appendChild(div);
+	const header = document.createElement("h2");
+	header.textContent = program.title + "" + program.version;
+	div.appendChild(header);
+	const description = document.createElement("p");
+	description.textContent = program.description;
+	div.appendChild(description);
+	const link = documnet.createElement("a");
+	link.href = program.file;
+	const button = document.createElement("button");
+	button.textContent = "Download";
+	link.appendChild(button);
+	div.appendChild(link);
 	programsContainer.appendChild(div);
 });
 */
