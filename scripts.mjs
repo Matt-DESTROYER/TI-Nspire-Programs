8ca739e5bb4deb0fefdc031ebd0922c2cdbc84d5
Matthew James <69026158+Matt-DESTROYER@users.noreply.github.com>
2022-02-20 12:16:47 +1100

    Update scripts.mjs

--- scripts.mjs
+++ scripts.mjs
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.7/firebase-app.js";
-
+import { getStorage } from "https://www.gstatic.com/firebasejs/9.6.7/firebase-storage.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAYc2AtdxlSEkD_VrGaIiKjOv0B3xD7uSs",
@@ -11,3 +11,4 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+const storage = getStorage(app);
