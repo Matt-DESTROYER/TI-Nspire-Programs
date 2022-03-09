3e00c43ef98b15f131c69d14c27353fbb9d6aa98
Matthew James <69026158+Matt-DESTROYER@users.noreply.github.com>
2022-03-10 10:45:11 +1100

    Update scripts.mjs

--- Articles/Post/scripts.mjs
+++ Articles/Post/scripts.mjs
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.7/firebase-app.js";
-import { getFirestore, collection, doc, getDoc, getDocs } from "https://www.gstatic.com/firebasejs/9.6.7/firebase-firestore.js";
+import { getFirestore, collection, doc, addDoc, updateDoc, getDocs } from "https://www.gstatic.com/firebasejs/9.6.7/firebase-firestore.js";
 
 const firebaseConfig = {
 	apiKey: "AIzaSyAYc2AtdxlSEkD_VrGaIiKjOv0B3xD7uSs",
@@ -22,65 +22,99 @@ async function GetCollection(collectionName) {
 	}
 }
 
-async function GetDocument(collectionName, documentName) {
+async function CreateDocument(collectionName, object) {
 	try {
-		const snap = await getDoc(doc(db, collectionName, documentName));
-		if (snap.exists()) {
-			return snap;
-		} else {
-			throw new Error("Document does not exist.");
-		}
+		return await addDoc(collection(db, collectionName), object);
 	} catch (err) {
+		console.error("Error writing to database:", err);
 		return err;
 	}
 }
 
-let loggedIn = false;
-(async function() {
-	(await GetCollection("Accounts")).forEach((account) => {
-		const data = account.data();
-		if (localStorage.getItem("username") === data.username &&
-		    localStorage.getItem("password") === data.password) {
-			loggedIn = true;
+async function UpdateDocument(collectionName, documentName, object) {
+	await updateDoc(doc(db, collectionName, documentName), object);
+}
+
+const title = document.getElementById("title");
+const version = document.getElementById("description");
+const description = document.getElementById("content");
+
+{
+	if (localStorage.getItem("title")) {
+		title.value = localStorage.getItem("title");
+		localStorage.removeItem("title");
+	}
+	if (localStorage.getItem("description")) {
+		description.value = localStorage.getItem("description");
+		localStorage.removeItem("description");
+	}
+	if (localStorage.getItem("content")) {
+		content.value = localStorage.getItem("content");
+		localStorage.removeItem("content");
+	}
+}
+
+const errormessage = document.getElementById("error-message");
+document.getElementById("upload").addEventListener("click", async () => {
+	if (localStorage.getItem("username") === null ||
+	    localStorage.getItem("password") === null) {
+		location.href = "https://matt-destroyer.github.io/TI-Nspire-Programs/Login/";
+	} else {
+		let loggedIn = false;
+		(await GetCollection("Accounts")).forEach((account) => {
+			const data = account.data();
+			if (localStorage.getItem("username") === data.username &&
+			    localStorage.getItem("password") === data.password) {
+				loggedIn = true;
+			}
+		});
+		if (!loggedIn) {
+			location.href = "https://matt-destroyer.github.io/TI-Nspire-Programs/Login/";
 		}
-	});
-})();
-
-let article_id;
-if (localStorage.getItem("article_id")) {
-	article_id = localStorage.getItem("article_id");
-	localStorage.removeItem("article_id");
-} else {
-	location.href = "https://matt-destroyer.github.io/TI-Nspire-Programs/Articles/";
-}
-
-const article = (await GetDocument("Articles", article_id)).data();
-
-const articlesContainer = document.getElementById("article");
-const div = document.createElement("div");
-div.classList.add("generic-container");
-articlesContainer.appendChild(div);
-const heading = document.createElement("div");
-if (loggedIn) {
-	const editButton = document.createElement("button");
-	editButton.textContent = "Edit";
-	editButton.classList.add("edit-button");
-	editButton.addEventListener("click", () => {
-		localStorage.setItem("title", article.title);
-		localStorage.setItem("description", article.description);
-		localStorage.setItem("content", article.content);
-		location.href = "https://matt-destroyer.github.io/TI-Nspire-articles/Articles/Post/";
-	});
-	heading.appendChild(editButton);
-}
-const header = document.createElement("h2");
-header.textContent = article.title;
-heading.appendChild(header);
-div.appendChild(heading);
-const author = document.createElement("p");
-author.textContent = "Published by: " + article.author;
-div.appendChild(author);
-const content = document.createElement("p");
-content.textContent = article.content;
-div.appendChild(content);
-articlesContainer.appendChild(div);
+	}
+	errormessage.hidden = true;
+	if (title.value.trim() === "") {
+		errormessage.textContent = "Error: No title.";
+		errormessage.hidden = false;
+	} else if (title.value.length > 100) {
+		errormessage.textContent = "Error: Title too long.";
+		errormessage.hidden = false;
+	} else if (description.value.trim() === "") {
+		errormessage.textContent = "Error: No description.";
+		errormessage.hidden = false;
+	} else if (description.value.length > 1000) {
+		errormessage.textContent = "Error: Description too long.";
+		errormessage.hidden = false;
+	} else if (content.value.trim() === "") {
+		errormessage.textContent = "Error: No content.";
+		errormessage.hidden = false;
+	} else if (content.length > 25000) {
+		errormessage.textContent = "Error: Content too long.";
+		errormessage.hidden = false;
+	} else {
+		let alreadyExists = false, id = null;
+		(await GetCollection("Articles")).forEach((program) => {
+			const data = program.data();
+			if (data.title === title.value) {
+				alreadyExists = true;
+				id = program.id;
+			}
+		});
+		if (alreadyExists) {
+			await UpdateDocument("Articles", id, {
+				"description": description.value,
+				"content": content.value,
+				"date": Date.now()
+			});
+		} else {
+			await CreateDocument("Articles", {
+				"title": title.value,
+				"author": atob(localStorage.getItem("username")),
+				"description": description.value,
+				"content": content.value,
+				"date": Date.now()
+			});
+		}
+		location.href = "https://matt-destroyer.github.io/TI-Nspire-Programs/Articles/";
+	}
+});
