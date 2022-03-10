0489f1d398aac79ce3f6728376a300f5b3d814a1
Matthew James <69026158+Matt-DESTROYER@users.noreply.github.com>
2022-03-10 20:30:00 +1100

    Update scripts.mjs

--- Articles/Post/scripts.mjs
+++ Articles/Post/scripts.mjs
@@ -54,9 +54,6 @@ const title = document.getElementById("title"),
 	}
 }
 
-// BASIC Markdown: https://github.com/Matt-DESTROYER/CDN/tree/main/JS/Markdown
-const Markdown=(function(){const e=/<\/?[a-zA-Z]+?.*?>/g;return function(n){n=n.replace(e,"");let t,r="";for(let e=0;e<n.length;e++)switch(n[e]){case"*":-1!==(t=n.indexOf("*",e+1))&&(-1===n.indexOf("\n",e+1)||n.indexOf("\n",e+1)>t)?(r+="<strong>"+n.substr(e+1,t-e-1)+"</strong>",e=t):r+=n[e];break;case"_":-1!==(t=n.indexOf("_",e+1))&&(-1===n.indexOf("\n",e+1)||n.indexOf("\n",e+1)>t)?(r+="<em>"+n.substr(e+1,t-e-1)+"</em>",e=t):r+=n[e];break;case"`":n[e+1]+n[e+2]==="``"?-1!==(t=n.indexOf("```",e+1))&&(r+="<code>"+t.substr(e+3,t-e-3)+"</code>"):-1!==(t=n.indexOf("`",e+1))?(r+="<code>"+n.substr(e+1,t-e-1)+"</code>",e=t):r+=n[e];break;default:r+=n[e]}return r}})();
-
 const errormessage = document.getElementById("error-message");
 document.getElementById("post").addEventListener("click", async () => {
 	if (localStorage.getItem("username") === null ||
