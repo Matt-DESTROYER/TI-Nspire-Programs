d41472049d514df93888aada0b1da99c1bcf4061
Matthew James <69026158+Matt-DESTROYER@users.noreply.github.com>
2022-03-11 18:42:43 +1100

    Update scripts.mjs

--- scripts.mjs
+++ scripts.mjs
@@ -85,7 +85,7 @@ async function renderPrograms() {
 		if (loggedIn) {
 			const editButton = document.createElement("button");
 			editButton.textContent = "Edit";
-			editButton.classList.add("edit-button");
+			editButton.classList.add("right-align");
 			editButton.addEventListener("click", () => {
 				localStorage.setItem("title", program.title);
 				localStorage.setItem("version", program.version);
