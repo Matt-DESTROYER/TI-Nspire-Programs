14b853a6945b257636a88396a28c559336ffa9c6
Matthew James <69026158+Matt-DESTROYER@users.noreply.github.com>
2022-03-11 18:41:30 +1100

    Update scripts.mjs

--- scripts.mjs
+++ scripts.mjs
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.7/firebase-app.js";
-import { getFirestore, collection, getDocs } from "https://www.gstatic.com/firebasejs/9.6.7/firebase-firestore.js";
+import { getFirestore, collection, doc, getDoc, getDocs, updateDoc } from "https://www.gstatic.com/firebasejs/9.6.7/firebase-firestore.js";
 
 const firebaseConfig = {
 	apiKey: "AIzaSyAYc2AtdxlSEkD_VrGaIiKjOv0B3xD7uSs",
@@ -22,6 +22,22 @@ async function GetCollection(collectionName) {
 	}
 }
 
+async function GetDocument(collectionName, documentName) {
+	try {
+		return await getDoc(doc(db, collectionName, documentName));
+	} catch (err) {
+		return err;
+	}
+}
+
+async function UpdateDocument(collectionName, documentName, object) {
+	try {
+		await updateDoc(doc(db, collectionName, documentName), object);
+	} catch (err) {
+		return err;
+	}
+}
+
 let loggedIn = false;
 (async function() {
 	(await GetCollection("Accounts")).forEach((account) => {
@@ -37,7 +53,11 @@ const sortSelect = document.getElementById("sort");
 sortSelect.addEventListener("change", renderPrograms);
 const programsContainer = document.getElementById("programs"), programs = [];
 (async function () {
-	(await GetCollection("Programs")).forEach((program) => programs.push(program.data()));
+	(await GetCollection("Programs")).forEach((program) => {
+		const data = program.data();
+		data.id = program.id;
+		programs.push(data);
+	});
 	renderPrograms();
 })();
 
@@ -65,7 +85,7 @@ async function renderPrograms() {
 		if (loggedIn) {
 			const editButton = document.createElement("button");
 			editButton.textContent = "Edit";
-			editButton.classList.add("right-align");
+			editButton.classList.add("edit-button");
 			editButton.addEventListener("click", () => {
 				localStorage.setItem("title", program.title);
 				localStorage.setItem("version", program.version);
@@ -73,8 +93,82 @@ async function renderPrograms() {
 				location.href = "https://matt-destroyer.github.io/TI-Nspire-Programs/Upload-Program/";
 			});
 			heading.appendChild(editButton);
+			const voteCounter = document.createElement("span");
+			voteCounter.textContent = program.votes;
+			heading.appendChild(voteCounter);
 			const upvoteButton = document.createElement("button");
+			const _votes = (await GetDocument("Accounts", program.id)).data().votes;
+			let id = null;
+			(await GetCollection("Accounts")).forEach((account) => {
+				const data = account.data();
+				if (data.username === localStorage.getItem("username") &&
+				    data.password === localStorage.getItem("password")) {
+					id = account.id;
+				}
+			});
+			upvoteButton.addEventListener(async () => {
+				const votes = (await GetDocument("Accounts", id)).data().votes;
+				let updateVote = false;
+				for (let i = 0; i < votes.length; i++) {
+					if (votes[i][0] === program.title && votes[i][1] === program.author) {
+						const prevVote = votes[i][2];
+						updateVote = true;
+						votes[i][2] = 1;
+						await UpdateDocument("Accounts", id, {
+							"votes": votes
+						});
+						await UpdateDocument("Programs", program.id, {
+							"votes": _votes + (prevVote === 1 ? 0 : 2)
+						});
+						break;
+					}
+				}
+				if (!updateVote) {
+					votes.push([ program.title, program.author, 1 ]);
+					await UpdateDocument("Accounts", id, {
+						"votes": votes
+					});
+					await UpdateDocument("Programs", program.id, {
+						"votes": _votes + 1;
+					});
+				}
+				voteCounter.textContent = (await GetDocument("Programs", program.id)).data().votes;
+			});
+			upvoteButton.classList.add("right-align");
+			upvoteButton.textContent = "Upvote";
+			heading.appendChild(upvoteButton);
 			const downvoteButton = document.createElement("button");
+			downvoteButton.addEventListener(async () => {
+				const votes = (await GetDocument("Accounts", id)).data().votes;
+				let updateVote = false;
+				for (let i = 0; i < votes.length; i++) {
+					if (votes[i][0] === program.title && votes[i][1] === program.author) {
+						const prevVote = votes[i][2];
+						updateVote = true;
+						votes[i][2] = -1;
+						await UpdateDocument("Accounts", id, {
+							"votes": votes
+						});
+						await UpdateDocument("Programs", program.id, {
+							"votes": _votes - (prevVote === -1 ? 0 : 2)
+						});
+						break;
+					}
+				}
+				if (!updateVote) {
+					votes.push([ program.title, program.author, -1 ]);
+					await UpdateDocument("Accounts", id, {
+						"votes": votes
+					});
+					await UpdateDocument("Programs", program.id, {
+						"votes": _votes - 1;
+					});
+				}
+				voteCounter.textContent = (await GetDocument("Programs", program.id)).data().votes;
+			});
+			downvoteButton.classList.add("right-align");
+			downvoteButton.textContent = "Downvote";
+			heading.appendChild(downvoteButton);
 		}
 		const header = document.createElement("h2");
 		header.textContent = program.title + " - " + program.version;
