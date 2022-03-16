9c8a3cc6a167fc212a54298cbe5dfd86bb8c8312
Matthew James <69026158+Matt-DESTROYER@users.noreply.github.com>
2022-03-16 19:02:09 +1100

    Update scripts.mjs

--- Login/scripts.mjs
+++ Login/scripts.mjs
@@ -28,22 +28,36 @@ const usernameInput = document.getElementById("username"),
 
 document.getElementById("login").addEventListener("click", async () => {
 	errormessage.hidden = true;
-	let accountData = {};
-	(await GetCollection("Accounts")).forEach((account) => {
-		const data = account.data();
-		if (usernameInput.value.trim() === atob(data.username)) {
-			accountData = data;
-		}
-	});
-	if (!accountData.hasOwnProperty("username")) {
+	if (usernameInput.value.trim() === "") {
+		errormessage.innerHTML = "Error: No username entered.";
+		errormessage.hidden = false;
+	} else if (usernameInput.value.trim().length > 50) {
 		errormessage.innerHTML = "Error: No user was found with this username.";
 		errormessage.hidden = false;
-	} else if (passwordInput.value.trim() === atob(accountData.password)) {
-		localStorage.setItem("username", accountData.username);
-		localStorage.setItem("password", accountData.password);
-		location.href = "https://matt-destroyer.github.io/TI-Nspire-Programs/";
-	} else {
+	} else if (passwordInput.value.trim() === "") {
+		errormessage.innerHTML = "Error: No password entered.";
+		errormessage.hidden = false;
+	} else if (passwordInput.value.trim().length < 8) {
 		errormessage.innerHTML = "Error: Incorrect password.";
 		errormessage.hidden = false;
+	} else {
+		let accountData = {};
+		(await GetCollection("Accounts")).forEach((account) => {
+			const data = account.data();
+			if (usernameInput.value.trim() === atob(data.username)) {
+				accountData = data;
+			}
+		});
+		if (!accountData.hasOwnProperty("username")) {
+			errormessage.innerHTML = "Error: No user was found with this username.";
+			errormessage.hidden = false;
+		} else if (passwordInput.value.trim() === atob(accountData.password)) {
+			localStorage.setItem("username", accountData.username);
+			localStorage.setItem("password", accountData.password);
+			location.href = "https://matt-destroyer.github.io/TI-Nspire-Programs/";
+		} else {
+			errormessage.innerHTML = "Error: Incorrect password.";
+			errormessage.hidden = false;
+		}
 	}
 });
