9bfc1ed6555629c46dd3902e72a11cdf3d40e26d
Matthew James <69026158+Matt-DESTROYER@users.noreply.github.com>
2022-03-18 12:17:40 +1100

    Update scripts.mjs

--- scripts.mjs
+++ scripts.mjs
@@ -74,6 +74,11 @@ const programsContainer = document.getElementById("programs"), programs = [];
 		const data = program.data();
 		data.id = program.id;
 		data.file = await GetFileURL(data.author + "/" + data.title, data.file);
+		const screenshots = data.screenshots;
+		data.screenshots = [];
+		for (const screenshot of screenshots) {
+			data.screenshots.push(await GetFileURL(data.author + "/" + data.title + "/Screenshots", screenshot));
+		}
 		programs.push(data);
 		await renderProgram(data);
 	});
