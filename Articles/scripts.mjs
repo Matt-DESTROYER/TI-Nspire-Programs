c41479142538c9bcde6a6c64bb30e9255bbdd3ce
Matthew James <69026158+Matt-DESTROYER@users.noreply.github.com>
2022-03-30 17:58:54 +1100

    Update scripts.mjs

--- Articles/scripts.mjs
+++ Articles/scripts.mjs
@@ -1,26 +1,4 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.8/firebase-app.js";
-import { getFirestore, collection, getDocs } from "https://www.gstatic.com/firebasejs/9.6.8/firebase-firestore.js";
-
-const firebaseConfig = {
-	apiKey: "AIzaSyAYc2AtdxlSEkD_VrGaIiKjOv0B3xD7uSs",
-	authDomain: "ti-nspire-programs-database.firebaseapp.com",
-	projectId: "ti-nspire-programs-database",
-	storageBucket: "ti-nspire-programs-database.appspot.com",
-	messagingSenderId: "1026879731613",
-	appId: "1:1026879731613:web:03c983f8072ebae67ba174"
-};
-
-const app = initializeApp(firebaseConfig);
-
-const db = getFirestore();
-
-async function GetCollection(collectionName) {
-	try {
-		return await getDocs(collection(db, collectionName));
-	} catch (err) {
-		return err;
-	}
-}
+import { GetCollection } from "https://Matt-DESTROYER.github.io/TI-Nspire-Programs/Database.mjs";
 
 let loggedIn = false;
 (async function() {
