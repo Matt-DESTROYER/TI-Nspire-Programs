d8f7db79f99446ecb89ed12d87f0abe1c2937a1d
Matthew James <69026158+Matt-DESTROYER@users.noreply.github.com>
2022-03-30 18:09:36 +1100

    Update scripts.mjs

--- Maze-Escape-Level-Editor/scripts.mjs
+++ Maze-Escape-Level-Editor/scripts.mjs
@@ -1,39 +1,4 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.8/firebase-app.js";
-import { getFirestore, collection, doc, addDoc, updateDoc, getDocs } from "https://www.gstatic.com/firebasejs/9.6.8/firebase-firestore.js";
-
-const firebaseConfig = {
-	apiKey: "AIzaSyAYc2AtdxlSEkD_VrGaIiKjOv0B3xD7uSs",
-	authDomain: "ti-nspire-programs-database.firebaseapp.com",
-	projectId: "ti-nspire-programs-database",
-	storageBucket: "ti-nspire-programs-database.appspot.com",
-	messagingSenderId: "1026879731613",
-	appId: "1:1026879731613:web:03c983f8072ebae67ba174"
-};
-
-const app = initializeApp(firebaseConfig);
-
-const db = getFirestore();
-
-async function GetCollection(collectionName) {
-	try {
-		return await getDocs(collection(db, collectionName));
-	} catch (err) {
-		return err;
-	}
-}
-
-async function CreateDocument(collectionName, documentName, object) {
-	try {
-		return await addDoc(collection(db, collectionName), object);
-	} catch (err) {
-		console.error("Error writing to database:", err);
-		return err;
-	}
-}
-
-async function UpdateDocument(collectionName, documentName, object) {
-	await updateDoc(doc(db, collectionName, documentName), object);
-}
+import { GetCollection, CreateDocument, UpdateDocument } from "https://Matt-DESTROYER.github.io/TI-Nspire-Programs/Database.mjs";
 
 const canvas = document.getElementById("screen");
 const ctx = canvas.getContext("2d", { alpha: false });
