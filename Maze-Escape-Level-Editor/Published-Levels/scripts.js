6c49a76f8b468125a9d63598cfa6d9ce30391d1c
Matthew James <69026158+Matt-DESTROYER@users.noreply.github.com>
2022-09-10 17:32:55 +1000

    Display ingame symbol

--- Maze-Escape-Level-Editor/Published-Levels/scripts.js
+++ Maze-Escape-Level-Editor/Published-Levels/scripts.js
@@ -24,6 +24,10 @@ for (const level of levels) {
 	const div = document.createElement("div");
 	div.classList.add("program-container");
 	const heading = document.createElement("div");
+	const ingame = document.createElement("span");
+	ingame.textContent = "In game: " + (level.ingame ? "✔️" : "❌");
+	ingame.classList.add("right-align");
+	div.append(ingame);
 	if (Account && btoa(level.author) === Account.username) {
 		const editButton = document.createElement("button");
 		editButton.textContent = "Edit";
