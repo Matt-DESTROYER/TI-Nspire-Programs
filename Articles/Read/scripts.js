9f3a0dd4e523cf65332c783a83c34bd1eb8d3574
Matthew James <69026158+Matt-DESTROYER@users.noreply.github.com>
2022-09-10 11:20:13 +1000

    Update and rename scripts.mjs to scripts.js

--- Articles/Read/scripts.js
+++ Articles/Read/scripts.js
@@ -1,4 +1,4 @@
-import { GetCollection, GetDocument } from "https://Matt-DESTROYER.github.io/TI-Nspire-Programs/Database.mjs";
+import { GetCollection, GetDocument } from "https://Matt-DESTROYER.github.io/TI-Nspire-Programs/Database.js";
 
 let loggedIn = false;
 (async function() {
